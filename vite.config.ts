dd95f267763be8dce0c400ba9a53f1fefb59019f
20020314 <yitianyangtwo@qq.com>
2022-10-12 13:56:52 +0000

    Edit vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     port: 8000,
     proxy: {
       "/api": {
-        target: "http://localhost:3000/",
+        target: "https://token.elysia.li/",
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
